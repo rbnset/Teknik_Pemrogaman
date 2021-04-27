92b61a5f5b049bcf9cb48eae5aa57d8c2a3958f8
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-04-27 14:21:58 +0700

    Program 5

---
+++ Topik3_Percabangan/program5.py
@@ -0,0 +1,14 @@
+nilai = input('Nilai Anda ? :')
+
+if nilai == 'A':
+    print('Pertahankan')
+elif nilai == 'B':
+    print('Harus lebih baik lagi')
+elif nilai == 'C':
+    print('Perbanyak belajar')
+elif nilai == 'D':
+    print('Jangan keseringan main')
+elif nilai == 'E':
+    print('Kebanyakan bolos...')
+else:
+    print('Maaf, format nilai tidak sesuai')
