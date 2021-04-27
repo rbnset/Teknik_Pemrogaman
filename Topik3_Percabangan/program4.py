153e5de43252deddde203f25395e13b011844213
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-04-27 14:16:25 +0700

    Program4

---
+++ Topik3_Percabangan/program4.py
@@ -0,0 +1,10 @@
+# Penggunaan IF ELSE
+
+lama_pinjam = int(input('Pinjaman buku beberpa hari? '))
+
+if lama_pinjam >= 3:
+    print('Meminjam buku selama ', lama_pinjam, 'hari dikenakan biaya')
+else:
+    print('Meminjam buku selama ', lama_pinjam, 'GRATIS')
+print()
+print('Terima kasih. Anda sudah meminjam Buku Kami')
