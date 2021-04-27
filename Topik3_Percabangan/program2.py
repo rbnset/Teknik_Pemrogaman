5e03ff774f097dd67da077ba5ed8b89ba10898d4
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-04-27 14:05:48 +0700

    program2

---
+++ Topik3_Percabangan/program2.py
@@ -0,0 +1,18 @@
+# Penentuan Diskon
+print('Penentuan Diskon Belanja')
+print('------------------------')
+
+besarPembelian = int(input('Nilai Nominal Pembelian : '))
+
+# Penentuan Diskon
+diskon = 0
+if besarPembelian >= 200000:
+    diskon = 0.05 * besarPembelian
+
+besarPembayaran = besarPembelian - diskon
+
+# Tampilkan Hasil
+print()
+print('Pembelian  = ', besarPembelian)
+print('Diskon     = ', diskon)
+print('Pembayaran = ', besarPembayaran)
