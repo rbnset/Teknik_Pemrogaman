226c6b4062ad1b361d82c1926d783ddebb2b9156
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-04-27 21:31:42 +0700

    TugasMandiri1

---
+++ Topik3_Percabangan/TugasMandiri1.py
@@ -0,0 +1,9 @@
+angka = int(input('Masukkan Angka : '))
+print('Angka : ', angka)
+
+if angka > 0:
+    print('Angka', angka, 'adalah Bilangan Positif')
+elif angka < 0:
+    print('Angka', angka, 'adalah Bilangan Negatif')
+else:
+    print('Angka', angka, 'adalah Bilangan Nol')
