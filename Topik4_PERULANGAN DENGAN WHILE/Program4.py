bacf045dec68fe70ece667f8e8b3cda1d8cd20cb
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-05-03 08:39:34 +0700

    program4

---
+++ Topik4_PERULANGAN DENGAN WHILE/Program4.py
@@ -0,0 +1,11 @@
+print('Deret N Bilangan Ganjil')
+print('--------------------------------')
+
+n = int(input('N = '))
+hitungan = 1
+
+while hitungan <= n:
+    print(2 * hitungan - 1, end=' ')
+    hitungan = hitungan + 1
+
+print()
