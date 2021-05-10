d7beeb699c2daeda9ff21b00c46389e191764c37
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-05-10 11:11:29 +0700

    Program3 for break

---
+++ Topik5_Perulangan Dengan For/Program3.py
@@ -0,0 +1,10 @@
+# contoh break
+# pada for
+
+for bilangan in range(1, 9):
+    if bilangan == 5:
+        break
+    print(bilangan)
+else:
+    print('*** Akhir For')
+    print('---- Selesai ----')
