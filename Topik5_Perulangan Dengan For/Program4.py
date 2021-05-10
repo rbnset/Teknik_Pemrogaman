5b09c9c8447c6fd9c5f06035894f3f2e1b4880e8
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-05-10 11:16:38 +0700

    Program 4 Contiune pada For

---
+++ Topik5_Perulangan Dengan For/Program4.py
@@ -0,0 +1,11 @@
+# contoh continue
+# pada for
+
+for bilangan in range(1, 9):
+    if bilangan == 5:
+        print('Continue dijalankan')
+        continue
+    print(bilangan)
+else:
+    print('*** Akhir For')
+print('---- Selesai ----')
