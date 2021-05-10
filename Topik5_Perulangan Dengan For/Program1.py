cd39b7f2af49cd49d8589aed0e67a6275bcea350
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-05-10 11:03:06 +0700

    Program 1 dan 2 penyajian deret n bilangan ganjil

---
+++ Topik5_Perulangan Dengan For/Program1.py
@@ -0,0 +1,9 @@
+# penyajian deret n bilagnan ganjil
+# mengguanakn for ver 1
+
+print('Deret N bilangan ganjil')
+print('-----------------------')
+n = int(input('N = '))
+for hitungan in range(1, n+1):
+    print(2*hitungan-1, end=' ')
+print()
---
+++ Topik5_Perulangan Dengan For/Program2.py
@@ -0,0 +1,9 @@
+# Penyajian deret n bilangan ganjil
+# mengguanakn for ver 2
+
+print('Deret N bilangan ganjil')
+print('-----------------------')
+n = int(input('N= '))
+for hitungan in range(1, 2*n, 2):
+    print(hitungan, end=' ')
+print()
