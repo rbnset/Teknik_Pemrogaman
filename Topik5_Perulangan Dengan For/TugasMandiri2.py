61d59be3efd3dbb4d09996a7fed81f316e4ef578
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-05-17 09:03:16 +0700

    Tugas Mandiri 1,2 & 3

---
+++ Topik5_Perulangan Dengan For/TugasMandiri2.py
@@ -0,0 +1,8 @@
+# Program untuk mencari total angka dari 1 s.d 10
+
+print('Program menghitung total angka dari 1 s.d. 10')
+print('--------------------------------------------')
+
+total = 0
+for i in range(1, 11):
+    print(i)
---
+++ Topik5_Perulangan Dengan For/TugasMandiri3.py
@@ -0,0 +1,7 @@
+# Program untuk menampilkan angka genap dari 2 s.d 20
+
+print('Program penampil angka genap dari 2 s.d 20')
+print('------------------------------------------')
+
+for i in range(2, 21, 2):
+    print(i)
