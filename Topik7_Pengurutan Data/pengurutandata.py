dffc89ab92f75139ee768a77d56ba7b535db76b4
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-07-02 10:23:09 +0700

    Program Pengurutab bana dengan string

---
+++ Topik7_Pengurutan Data/pengurutandata.py
@@ -0,0 +1,8 @@
+# Program Pengurutan Nama dengan String
+
+nama = 'Robin Setiyawan', 'Tadem Virgi', 'Deny Irawan', 'Destenty Manabung', 'Della Rizky', 'Sonia Anggraini', 'Nimas Dewi Ayu',
+
+# Menggunakan Fungsi Shorted
+
+for urut in sorted(nama):
+    print(urut)
