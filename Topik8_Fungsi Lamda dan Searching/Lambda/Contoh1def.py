d9c8fb6006618dcd449faf9a0434e4b867fe40a1
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-07-03 07:42:28 +0700

    topik 8 Lamda dan testing dengan jyupiter

---
+++ Topik8_Fungsi Lamda dan Searching/Lambda/Contoh1def.py
@@ -0,0 +1,14 @@
+def kuadrat(x):
+    return x**2
+
+
+fungsi = kuadrat(3)
+print(fungsi)
+
+
+def kali(x, y):
+    return x*y
+
+
+fungsi = kali(4, 3)
+print(fungsi)
--- Topik8_Fungsi Lamda dan Searching/Lamda/Contoh1def.py
+++
@@ -1,8 +0,0 @@
-def kuadrat(x):
-    return x**2
-    kuadrat(3)
-
-
-def kali(x, y):
-    return x*y
-    kali(4, 3)
