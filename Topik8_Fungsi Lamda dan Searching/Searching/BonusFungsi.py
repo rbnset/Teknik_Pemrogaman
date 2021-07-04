d56401e5af98f7683f42736162bb5fba8f826d14
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-07-04 07:17:44 +0700

    Bonus Fungsi pada Searching

---
+++ Topik8_Fungsi Lamda dan Searching/Searching/BonusFungsi.py
@@ -0,0 +1,10 @@
+# Contoh cari nilai maksimal dari setuap element list
+# inisialisasi nilai dalam list dengan variabel angka
+
+angka = [3, 45, 22, 10]
+
+# Cari nilai maksimal dari dalam list variabel angka
+print(max(angka))
+
+# Cari nilai minimal dari dalam list variavel angka
+print(min(angka))
