4c63d841b6e9dba6186f3c88ed59055e0eb84f30
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-07-19 09:58:29 +0700

    Contoh Program Pewarisan 3, namun dengan cara menurunkan data dari class pertama kepada yang kedua

---
+++ TOpik12_PBO/Contoh3.2.py
@@ -0,0 +1,22 @@
+# Contoh Program Pewarisan 3, namun dengan cara menurunkan data dari class pertama kepada yang kedua :
+
+class Mahasiswa():
+    def __init__(self, nama, nim, alamat):
+        self.nama = nama
+        self.nim = nim
+        self.alamat = alamat
+
+    def cek_data_mahasiswa(self):
+        print('nama : ', self.nama, '\nnim : ',
+              self.nim, '\nalamat : ', self.alamat)
+
+
+class Dosen(Mahasiswa):
+    pass
+
+
+mahasiswa1 = Mahasiswa('irawan', '08122671', 'jakarta barat')
+mahasiswa1.cek_data_mahasiswa()
+
+dosen = Dosen('riko', '08126672', 'jakarta selatan')
+dosen.cek_data_mahasiswa()
